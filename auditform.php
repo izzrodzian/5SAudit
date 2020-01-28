646f4f0f64fa6b0d2b9ed4e62a93598556a713ae
izzrodzian <48191059+izzrodzian@users.noreply.github.com>
2020-01-28 13:03:29 +0800

    update audit form, add section 1 draft file

---
+++ auditform.php
@@ -0,0 +1,16 @@
+<php?
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+
+	<body>
+		<br>
+		<center><h1><p>AUDIT SISTEM AMALAN 5S</p></h1>
+		<img src="./dist/img/5s.jpg" alt="Amalan 5S" width="500" height="450">	
+	
+
+	</body>
+</html>
---
+++ section1form.php
@@ -0,0 +1,33 @@
+<php?
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+
+	<body>
+		<br><br>
+		<form>
+		<center><h1><p>BORANG AUDIT SISTEM AMALAN 5S</p></h1>
+		<br>
+		SEKSYEN 1: KEPERLUAN UTAMA PERLAKSANAAN
+		<br><br>
+		</center>
+		<table align="center">
+			<div class="form-group">
+			<tr>
+				<td>Email Address: </td>
+				<td><input type="email" class="form-control" name="userEmail" placeholder="Enter your email"></td>
+			</tr>
+			</div>
+			<div class="form-group">
+			<tr>
+				<td>Full Name: </td>
+				<td><input type="email" class="form-control" name="userEmail" placeholder="Enter your email"></td>
+			</tr>
+			</div>
+
+		</table>
+		</form>
+	
