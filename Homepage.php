26c24ddb92ef9b4c8b34e92d637d8ded34533ef8
ainmrn <ainmrn.work@gmail.com>
2020-02-05 14:51:46 +0800

    System Interfaces
    Interface of Homepage, Login, Inspection, Report

---
+++ Homepage.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.12.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5s-122x125.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Homepage</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rPwfwJELGC" once="menu" id="menu1-4">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    
+                         <img src="assets/images/logo5s-122x125.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://mobirise.co">
+                        5S AUDIT INSPECTION</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="https://mobirise.co" aria-expanded="true"><span class="mbrib-left mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        BACK</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link link dropdown-toggle text-white display-4" data-toggle="dropdown-submenu" aria-expanded="true"><span class="mobi-mbri mobi-mbri-logout mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        LOGOUT</a><div class="dropdown-menu"><a class="dropdown-item text-white display-4">PROFILE</a></div>
+                </li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary display-4" href="Login.html"><span class="mbrib-users mbr-iconfont mbr-iconfont-btn"></span>
+                    
+                    LOGIN</a></div>
+        </div>
+    </nav>
+</section>
+
+<section class="counters3 counters cid-rPwiIK95ry" id="counters3-6">
+ 
+
+    <div class="container pt-4 mt-2">
+        <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2">
+                    AMPANG LINE
+        </h2>
+        
+        <div class="media-container-row">
+            <div class="media-block m-auto" style="width: 49%;">
+                <div class="mbr-figure">
+                    <img src="assets/images/background6.jpg">
+                </div>
+            </div>
+            <div class="cards-block">
+                <div class="cards-container">
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex align-items-center">
+                            <div class="card-img pr-3 d-flex align-items-center align-left">
+                                
+                                <h4 class="py-3 mbr-fonts-style display-2">
+                                    Sort
+                                </h4>
+                            </div>
+                            <div class="card-text">
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                    Remove what is not needed and keep what is needed.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex align-items-center">
+                            <div class="card-img pr-3 d-flex align-items-center align-left">
+                                
+                                <h3 class="py-3 mbr-fonts-style display-2">
+                                    Set in Order
+                                </h3>
+                            </div>
+                            <div class="card-text">
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                    Arrange essential items in order for easy access.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex align-items-center">
+                            <div class="card-img pr-3 d-flex align-items-center align-left">
+                                
+                                <h3 class="py-3 mbr-fonts-style display-2">
+                                    Shine
+                                </h3>
+                            </div>
+                            <div class="card-text">
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                        Keep things clean and tidy; no trash or dirt in the workplace.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex align-items-center">
+                            <div class="card-img pr-3 d-flex align-items-center align-left">
+                                
+                                <h3 class="py-3 mbr-fonts-style display-2">
+                                    Standardize
+                                </h3>
+                            </div>
+                            <div class="card-texts">
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                       Establish standards and guidelines to maintain a clean workplace.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card px-3 align-left col-12">
+                        <div class="panel-item p-4 d-flex align-items-center">
+                            <div class="card-img pr-3 d-flex align-items-center align-left">
+                                
+                                <h3 class="py-3 mbr-fonts-style display-2">
+                                    Sustain
+                                </h3>
+                            </div>
+                            <div class="card-texts">
+                                <p class="mbr-content-text mbr-fonts-style display-7">
+                                       Make 5S a habit and teach others to adhere to established standards.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-slider-carousel cid-rPwj5Ibhrs" id="gallery4-8">
+
+    
+
+    <div class="container">
+        <div><!-- Gallery --><div class="gallery-row"><div class="mbr-gallery-layout-default"><div><div class="row-first row justify-content-center m-0"><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="0" data-toggle="modal"><img src="assets/images/background1.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="1" data-toggle="modal"><img src="assets/images/background2.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="2" data-toggle="modal"><img src="assets/images/background3.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div></div><div class="row-second row justify-content-center m-0"><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="3" data-toggle="modal"><img src="assets/images/background4.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="4" data-toggle="modal"><img src="assets/images/background5.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="5" data-toggle="modal"><img src="assets/images/background6.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-8" data-slide-to="6" data-toggle="modal"><img src="assets/images/background7.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Type caption here</span></div></div></div></div><div class="clearfix"></div></div></div><!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery4-8"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="carousel-inner"><div class="carousel-item active"><img src="assets/images/background1.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background2.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background3.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background4.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background5.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background6.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background7.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/background8.jpg" alt="" title=""></div></div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery4-8"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery4-8"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div></div></div></div></div>
+    </div>
+
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+  <script src="assets/gallery/player.min.js"></script>
+  <script src="assets/gallery/script.js"></script>
+  
+  
+</body>
+</html>
---
+++ Login.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.12.0, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.0, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5s-122x125.png" type="image/x-icon">
+  <meta name="description" content="Website Builder Description">
+  
+  <title>Login</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rPwfwJELGC" once="menu" id="menu1-b">
+
+
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+
+      <div class="menu-logo">
+        <div class="navbar-brand">
+          <span class="navbar-logo">
+
+           <img src="assets/images/logo5s-122x125.png" alt="Mobirise" title="" style="height: 3.8rem;">
+
+         </span>
+         <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="https://mobirise.co">
+         5S AUDIT INSPECTION</a></span>
+       </div>
+     </div>
+   </nav>
+ </section>
+
+ <section class="header15 cid-rPxhb253yR mbr-fullscreen " id="header15-g">
+
+  <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(7, 59, 76);"></div>
+  <div class="container align-center">
+ <center><div class="mbr-white content-container">
+        <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">USER LOGIN</h1>
+      </div></center>
+    <div class="form-container">
+      <div class="media-container-column" data-form-type="formoid">
+      
+      <form method="POST">
+        <div class="container">
+          <div>
+            <input class="form-control px-3 display-7 align-center" type="text" name="idno" placeholder="Enter your ID no" required="">
+          </div></br>
+
+          <input class="form-control px-3 display-7 align-center" type="password" name="password" placeholder="Enter your password" required="">
+        </div></br>
+
+        <div>
+          <input class="form-control px-3 display-7 align-center" list="roletype" name ="roletype" placeholder="Select role type">
+          <datalist id="roletype">
+            <option value="Admin"></option>
+            <option value="Auditor"></option>
+            <option value="Guest"></option>
+          </datalist> 
+        </div></br>
+
+        <div>
+         <a href="homepage.php"><input class="btn btn-sm btn-primary btn-form display-4" type="submit" name="cancel" value="Cancel">
+
+          <input class="btn btn-sm btn-primary btn-form display-4"  type="submit" name="login" value="Login">
+        </div>
+      </div>
+
+    </form>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<section once="footers" class="cid-rPwjkLZhDD" id="footer7-c">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"></li></ul>
+            </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.youtube.com/c/mobirise" target="_blank">
+                            <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://instagram.com/mobirise" target="_blank">
+                            <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                            <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.behance.net/Mobirise" target="_blank">
+                            <span class="socicon-behance socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Copyright 2020 | 5S Audit Inspection | Ampang Line - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+</body>
+</html>
